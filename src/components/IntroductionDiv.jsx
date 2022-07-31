ed4263e6ff559e804f04a4ae9300cf0b6a642c5b
Minh Bui <ultraminh2709@gmail.com>
2022-07-31 12:34:17 -0700

    pretty much finish the introductiondiv page with the crawling animation

--- src/components/IntroductionDiv.jsx
+++ src/components/IntroductionDiv.jsx
@@ -18,34 +18,66 @@ function ScrollingText(props) {
   const ref = useRef()
   const scroll = useScroll()
   return (
-    <motion.div
-      animate={props.startScroll}
-      initial={{
-        rotateX: '20deg',
-        y: '100px',
-        transformPerspective: '400px',
-      }}
-    >
-      <motion.div className='scrollingText flex flex-col'>
-        {props.text.split('\n').map((i) => {
-          // console.log(i)
-          return (
-            <p key={i} className='flex justify-center text-7xl'>
-              {i}
-            </p>
-          )
-        })}
+    <section className='star-wars'>
+      <motion.div
+        animate={props.startScroll}
+        className='crawl'
+        initial={{
+          y: '-100px',
+          translateZ: '0px',
+          rotateX: '20deg',
+        }}
+      >
+        <motion.div className='flex flex-col'>
+          <p className='flex justify-center text-7xl'> Inspired by Star Wars</p>
+          <p className='flex justify-center text-7xl'>(Click the button)</p>
+          <div className='h-8' />
+          {props.text.split('\n').map((i) => {
+            // console.log(i)
+            return (
+              <p key={i} className='flex justify-center'>
+                {i}
+              </p>
+            )
+          })}
+        </motion.div>
       </motion.div>
-    </motion.div>
+    </section>
   )
 }
 const IntroductionDiv = () => {
   const startScroll = useAnimationControls()
   const [currentButton, setButton] = useState('start')
+  const [buttonColor, setColor] = useState('white')
   const introText = `Hi, my name is Minh Bui
 	I'm currently a student studying 
 	Computer Engineering at UCSB
-	aaaaaaaaaaaaaaaaaaaaaaaaaaaa
+	Some of my hobbies include
+	coding, gaming, and playing 
+	sports with my good friends.
+
+	I also enjoy scifi movies 
+	(such as Star Wars very much)
+	because I think that they are
+	an excellent catalyst for growth
+	of imagination.
+
+	I went into Computer Engineering
+	because of my particular love for 
+	robotics, and both the hardware and
+	software engineering side that robotics
+	includes.
+
+	I endeavor to be a great software/hardware
+	engineer that will leave impressionable 
+	results that will inspire the 
+	young generation, as I 
+	was inspired when I was younger.
+
+	Note: Because the nature of the
+	framer-motion framework, you need to 
+	reload the page in order to reset the text
+	to the original position
 	`
   const ref = useRef(null)
   return (
@@ -61,43 +93,53 @@ const IntroductionDiv = () => {
           <ambientLight intensity={1} />
           <Suspense fallback={null}>
             <Html fullscreen>
-              <div className='m-2 h-8 w-8 bg-blue-400'>
-                {currentButton == 'start' ? (
-                  <FontAwesomeIcon
-                    className='icons'
-                    icon={faPlay}
-                    color='white'
-                    onClick={() => {
-                      setButton('stop')
-                      startScroll.start({
-                        translateZ: '-1000px',
-                        transition: { duration: 5 },
-                      })
-                    }}
-                  />
-                ) : (
-                  <FontAwesomeIcon
-                    className='icons'
-                    icon={faStop}
-                    color='white'
-                    onClick={() => {
-                      setButton('start')
-                      startScroll.stop()
-                    }}
-                  />
-                )}
+              <div class='fade'>
+                <div className='relative m-2 top-24 z-10 h-16 w-16'>
+                  {currentButton == 'start' ? (
+                    <FontAwesomeIcon
+                      size='2x'
+                      className='icons'
+                      icon={faPlay}
+                      color={buttonColor}
+                      onMouseOver={() => setColor('#feda4a')}
+                      onMouseOut={() => setColor('white')}
+                      onClick={() => {
+                        setButton('stop')
+                        startScroll.start({
+                          y: '-6000px',
+                          translateZ: '-2500px',
+                          rotateX: '25deg',
+                          transition: { duration: 60 },
+                        })
+                      }}
+                    />
+                  ) : (
+                    <FontAwesomeIcon
+                      size='2x'
+                      className='icons'
+                      onMouseOver={() => setColor('#feda4a')}
+                      onMouseOut={() => setColor('white')}
+                      icon={faStop}
+                      color={buttonColor}
+                      onClick={() => {
+                        setButton('start')
+                        startScroll.stop()
+                      }}
+                    />
+                  )}
+                </div>
               </div>
-       {/*        <div className='m-2 h-8 w-8 bg-blue-400'> */}
-       {/*          <FontAwesomeIcon */}
-       {/*            className='icons' */}
-       {/*            icon={faRedo} */}
-       {/*            color='white' */}
-       {/*            onClick={() => { */}
-					  {/* console.log('clicked') */}
-       {/**/}
-       {/*            }} */}
-       {/*          /> */}
-       {/*        </div> */}
+              {/*        <div className='m-2 h-8 w-8 bg-blue-400'> */}
+              {/*          <FontAwesomeIcon */}
+              {/*            className='icons' */}
+              {/*            icon={faRedo} */}
+              {/*            color='white' */}
+              {/*            onClick={() => { */}
+              {/* console.log('clicked') */}
+              {/**/}
+              {/*            }} */}
+              {/*          /> */}
+              {/*        </div> */}
               <ScrollingText startScroll={startScroll} text={introText} />
             </Html>
             <Stars position={[0, -5, 0]} />
