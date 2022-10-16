b5c644214e005b0be1208aa97df42ad9090a6050
Minh Bui <ultraminh2709@gmail.com>
2022-10-15 22:09:32 -0700

    current changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
 import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
-
 import tailwind from "@astrojs/tailwind";
+
+import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind()]
+  integrations: [react(), tailwind()],
+  adapter: netlify()
 });
-
-
