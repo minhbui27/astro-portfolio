a1d4a6c49c1c284b5e2b15899d7ff7524bf6062f
Minh Bui <ultraminh2709@gmail.com>
2022-10-23 22:31:42 -0700

    this should do it

--- src/components/Introduction.tsx
+++
@@ -1,8 +0,0 @@
-import * as React from 'react'
-import './Introduction.scss'
-export default function Introduction() {
-	return(
-		<div className="introduction min-h-screen w-full">
-		</div>
-	)
-}
