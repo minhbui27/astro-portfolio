806ec6c08c33346eed88d2f661388da9588baa41
Minh Bui <ultraminh2709@gmail.com>
2022-10-23 22:00:34 -0700

    pls work

--- src/components/IntroductionDiv.tsx
+++ src/components/IntroductionDiv.tsx
@@ -5,7 +5,7 @@ import {
   AnimatePresence,
   useAnimationControls,
 } from 'framer-motion'
-import '../styles/IntroductionDiv.scss'
+import '../styles/Scroll.scss'
 import { Canvas } from '@react-three/fiber'
 import {  Html } from '@react-three/drei'
 import { Stars } from './Welcome'
