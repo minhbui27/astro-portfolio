b808c4b121204f4654d0661a26c7bce89bf6854c
Minh Bui <ultraminh2709@gmail.com>
2023-01-10 19:19:24 -0800

    added some new content and images, need to add description for project items

--- src/components/ProjectsDiv.tsx
+++ src/components/ProjectsDiv.tsx
@@ -3,26 +3,30 @@ import Card from '../layouts/Card'
 import { motion } from 'framer-motion'
 import './ProjectsDiv.scss'
 const ProjectDiv = (): JSX.Element => {
-  const ids: number[] = [1, 2, 3, 4, 5, 6]
   return (
-    <motion.div
-      id='projects'
-      className='p-8 bg-blue-900 flex flex-col h-screen'
-    >
-      <div className='h-16 w-full'></div>
-      <motion.div className='projectsText text-white text-3xl md:text-6xl lg:text-7xl'>
+    <div id='projects' className='bg-blue-900 flex flex-col min-h-screen'>
+      <motion.div className='mt-16 projectsText text-white text-3xl md:text-6xl lg:text-7xl'>
         PROJECTS
       </motion.div>
 
-      <div className='w-full h-auto md:h-full items-center'>
-        <div className='flex flex-nowrap items-start h-full overflow-x-scroll'>
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-        </div>
+      <div className='flex flex-col md:flex-row card-container'>
+        <Card
+          image='digital_oscillator.jpg'
+          title='Bistable Computer Clock'
+          content=''
+        />
+        <Card
+          image='discord_bot.png'
+          title='Math Discord Bot'
+          content=''
+        />
+        <Card
+          image='rc_car.jpg'
+          title='RC LEGO Car'
+          content=''
+        />
       </div>
-    </motion.div>
+    </div>
   )
 }
 export default ProjectDiv
--- src/layouts/Card.tsx
+++ src/layouts/Card.tsx
@@ -1,12 +1,20 @@
 import * as React from 'react'
-import {motion} from 'framer-motion'
-const Card = ({...props}): JSX.Element => {
-	return(
-		<motion.div className='opacity-90 mx-4 w-1/3 h-full bg-blue-300 flex-none'>
-			<motion.div className="text-center w-full h-1/3 text-6xl">
-				test
-			</motion.div>
-		</motion.div>
-	)
+import { motion } from 'framer-motion'
+import '../styles/Card.scss'
+const Card = ({ ...props }): JSX.Element => {
+  return (
+    <motion.div className='card py-16 px-4 w-full md:w-1/3 flex-none'>
+      <div className='w-full card-content bg-blue-900 opacity-90 rounded-3xl'>
+        <img
+          className='image-height h-64 w-full object-cover rounded-3xl'
+          src={`/project_images/${props.image}`}
+        />
+        <div className='my-2 card-title md:text-3xl w-full flex flex-row justify-center text-center text-white'>
+          {props.title}
+        </div>
+        <div className='h-3/5 w-full'>{props.content}</div>
+      </div>
+    </motion.div>
+  )
 }
 export default Card
