fd926a67a9cfdf76d4d535d366d619d9abb81d90
Minh Bui <ultraminh2709@gmail.com>
2023-08-05 23:09:07 -0700

    added stuff in projects

--- src/components/ProjectsDiv.tsx
+++ src/components/ProjectsDiv.tsx
@@ -6,25 +6,25 @@ const ProjectDiv = (): JSX.Element => {
   return (
     <div id='projects' className='bg-blue-900 flex flex-col min-h-screen'>
       <motion.div className='mt-16 projectsText text-white text-3xl md:text-6xl lg:text-7xl'>
-        PROJECTS
+        Some of my projects
       </motion.div>
 
-      <div className='flex flex-col md:flex-row card-container'>
-        <Card
-          image='digital_oscillator.jpg'
-          title='Bistable Computer Clock'
-          content=''
-        />
-        <Card
-          image='discord_bot.png'
-          title='Math Discord Bot'
-          content=''
-        />
-        <Card
-          image='rc_car.jpg'
-          title='RC LEGO Car'
-          content=''
-        />
+      <div className='px-8 flex flex-col md:flex-row justify-center card-container'>
+					<Card
+						image='digital_oscillator.jpg'
+						title='Bistable Clock'
+						content='A clock designed to be used in a breadboard computer made from a 555-timer chip and an RC circuit, which in conjunction simulates a square wave oscillator that can operate in astable and monostable modes, and reach up to 60Mhz.'
+					/>
+					<Card
+						image='discord_bot.png'
+						title='Math Discord Bot'
+						content='A bot written with the purpose of integrating mathematical operations with the discord client. Capable of performing multiple matrix operations such as rref, calculus operations such as differentiation, and such operations are displayed through a LaTeX api as an image.'
+					/>
+					<Card
+						image='rc_car.jpg'
+						title='RC LEGO Car'
+							content='Converted a LEGO Technic 24hr racing car into a bluetooth controlled car, using an Arduino board, motor controllers, DC motors, custom mounting of the drive shaft and steering rack using 3d printed parts.'
+					/>
       </div>
     </div>
   )
--- src/layouts/Card.tsx
+++ src/layouts/Card.tsx
@@ -3,16 +3,17 @@ import { motion } from 'framer-motion'
 import '../styles/Card.scss'
 const Card = ({ ...props }): JSX.Element => {
   return (
-    <motion.div className='card py-16 px-4 w-full md:w-1/3 flex-none'>
-      <div className='w-full card-content bg-blue-900 opacity-90 rounded-3xl'>
+    <motion.div className='h-[35rem] md:h-[40rem] my-2 md:my-8 px-4 w-full md:w-[20rem] lg:w-96 flex-none'>
+			{/*bg-zinc-300/300 indicates the color zinc-300 with opacity 30%*/}
+      <div className='h-full w-full card-content bg-zinc-900/80 rounded-xl'>
         <img
-          className='image-height h-64 w-full object-cover rounded-3xl'
+          className='image-height h-64 w-full object-cover rounded-t-xl'
           src={`/project_images/${props.image}`}
         />
         <div className='my-2 card-title md:text-3xl w-full flex flex-row justify-center text-center text-white'>
           {props.title}
         </div>
-        <div className='h-3/5 w-full'>{props.content}</div>
+        <div className='text-white text-md md:text-lg lg:text-xl px-8 h-3/5 w-full'>{props.content}</div>
       </div>
     </motion.div>
   )
